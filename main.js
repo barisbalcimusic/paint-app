b8802eabd50b7ec20ce14478422c16728c0e0e06
hannahnier <hannahr@posteo.de>
2024-03-19 14:02:38 +0100

    add switch for selecting tool

--- main.js
+++ main.js
@@ -3,17 +3,6 @@ const sidebar = document.querySelector(".sidebar");
 const tools = document.querySelectorAll(".tool");
 const field = document.getElementById("field");
 
-//select any tool
-sidebar.firstElementChild.addEventListener("click", (e) => {
-  if (e.target !== e.currentTarget) {
-    tools.forEach((el) => {
-      el.style.color = "unset";
-    });
-    e.target.style.color = "lightblue";
-    field.style.cursor = `url('./img/${e.target.id}.png'), auto`;
-  }
-});
-
 //create canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -23,24 +12,59 @@ canvas.height = window.innerHeight - 50;
 //some initial status
 let isDrawing = false;
 
-//drawing
-field.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  ctx.beginPath();
-  ctx.moveTo(e.clientX - 50, e.clientY - 50);
-});
+//select any tool
+sidebar.firstElementChild.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) {
+    tools.forEach((el) => {
+      el.style.color = "unset";
+    });
+    e.target.style.color = "lightblue";
+    field.style.cursor = `url('./img/${e.target.id}.png'), auto`;
 
-field.addEventListener("mousemove", (e) => {
-  if (isDrawing) {
-    ctx.lineTo(e.clientX - 50, e.clientY - 50);
-    ctx.stroke();
+    switch (e.target.id) {
+      case "pen":
+        draw();
+        break;
+      case "eraser":
+        break;
+      case "select":
+        break;
+      case "shapes":
+        break;
+      case "text":
+        break;
+      case "zoom-in":
+        break;
+      case "zoom-out":
+        break;
+      case "highlighter":
+        break;
+      default:
+        break;
+    }
   }
 });
 
-field.addEventListener("mouseup", (e) => {
-  isDrawing = false;
-  ctx.strokeStyle = null;
-});
+//drawing
+function draw() {
+  field.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - 50, e.clientY - 50);
+  });
+
+  field.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+      ctx.lineTo(e.clientX - 50, e.clientY - 50);
+      ctx.stroke();
+    }
+  });
+
+  field.addEventListener("mouseup", (e) => {
+    isDrawing = false;
+    ctx.strokeStyle = null;
+  });
+}
 
 //color change
 const colorPalette = main.querySelector(".color-palette");
