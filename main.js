e585392b8e0ee65ff5b7bb1740e494619fe4a4a0
hannahnier <hannahr@posteo.de>
2024-03-20 15:44:24 +0100

    resolve merge conflicts (in order to keep the currently unfinished code for shape-tool)

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ window.addEventListener("resize", () => {
 //some initial status
 let isDrawing = false;
 let currentColor = "black";
+let shapesToggle = false;
 
 //color change
 const colorPalette = main.querySelector(".color-palette");
@@ -38,30 +39,44 @@ sidebar.firstElementChild.addEventListener("click", (e) => {
     switch (e.target.id) {
       case "pen":
         field.style.cursor = "crosshair";
+        unselectShape();
         draw();
         break;
       case "eraser":
         field.style.cursor = `url("./img/eraser.png"), auto`;
+        unselectShape();
         erase();
         break;
       case "select":
+        unselectShape();
         break;
       case "shapes":
-        field.style.cursor = "crosshair";
+        field.style.cursor = "auto";
+        if (!shapesToggle) {
+          shapesToggle = true;
+          selectShape();
+        } else {
+          unselectShape();
+        }
         break;
       case "text":
         field.style.cursor = "text";
+        unselectShape();
         addText();
         break;
       case "zoom-in":
         field.style.cursor = "zoom-in";
+        unselectShape();
         break;
       case "zoom-out":
         field.style.cursor = "zoom-out";
+        unselectShape();
         break;
       case "highlighter":
+        unselectShape();
         break;
       default:
+        unselectShape();
         break;
     }
   }
@@ -147,4 +162,83 @@ function convertToCanvasText(fSize, tColor, fFamily, x, y, textField) {
     ctx.fillText(textField.value, x + 5, y + 20);
     textField.remove();
   });
+}
+
+//unselecting shapes
+function unselectShape() {
+  shapesToggle = false;
+  main.querySelector("#shapes").style.color = "unset";
+  main.querySelectorAll(".shapeLabel").forEach((label) => {
+    label.style.display = "none";
+    label.style.color = "black";
+  });
+}
+
+//selecting a shape
+function selectShape() {
+  main.querySelectorAll(".shapeLabel").forEach((label) => {
+    label.style.display = "block";
+    label.addEventListener("click", (e) => {
+      unselectShape();
+      drawShape(e.target.getAttribute("for"));
+    });
+  });
+}
+
+//drawing a shape (rectangle or ellipse)
+function drawShape(chosenShape) {
+  field.style.cursor = "crosshair";
+  let startX, startY;
+  let width, height;
+  let shape;
+  field.addEventListener("mousedown", mouseDownHandler);
+
+  function mouseDownHandler(e) {
+    startX = e.clientX;
+    startY = e.clientY;
+    shape = document.createElement("div");
+    shape.style.position = "absolute";
+    shape.style.border = `1px solid ${currentColor}`;
+    shape.style.left = startX + "px";
+    shape.style.top = startY + "px";
+    main.appendChild(shape);
+    if (chosenShape !== "rectangle") {
+      shape.style.borderRadius = "50%";
+    }
+
+    field.addEventListener("mousemove", mouseMoveHandler);
+  }
+
+  function mouseMoveHandler(e) {
+    width = e.clientX - startX;
+    height = e.clientY - startY;
+    shape.style.width = width + "px";
+    shape.style.height = height + "px";
+    field.addEventListener("mouseup", mouseUpHandler);
+  }
+
+  function mouseUpHandler() {
+    field.removeEventListener("mousedown", mouseDownHandler);
+    field.removeEventListener("mousemove", mouseMoveHandler);
+    field.removeEventListener("mouseup", mouseUpHandler);
+    field.style.cursor = "default";
+    shape.remove();
+    let centerX = startX + width / 2;
+    let centerY = startY + height / 2;
+    ctx.beginPath();
+    ctx.strokeStyle = currentColor;
+    ctx.lineWidth = 0.5;
+    chosenShape === "rectangle"
+      ? ctx.rect(startX, startY, width, height)
+      : ctx.ellipse(
+          centerX,
+          centerY,
+          Math.abs(width) / 2,
+          Math.abs(height) / 2,
+          0,
+          0,
+          2 * Math.PI
+        );
+    ctx.stroke();
+  }
 }
